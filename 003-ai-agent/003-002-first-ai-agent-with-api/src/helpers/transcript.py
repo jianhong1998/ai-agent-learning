cfcb1af50f8e99e2eb2a49d1f4c53661690a7cb6
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2026-01-12 08:47:18 +0800

    create 002 with api endpoint

---
+++ 003-ai-agent/003-002-first-ai-agent-with-api/src/helpers/transcript.py
@@ -0,0 +1,15 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import urlparse, parse_qs
+
+def get_transcript(url: str, languages: list[str] = ['en']) -> str:
+  url_queries = parse_qs(urlparse(url).query)
+  video_id = url_queries.get('v', [''])[0]
+
+  print(f"Video ID: {video_id}\nLanguages: {languages}")
+
+  api = YouTubeTranscriptApi()
+
+  res = api.fetch(video_id, languages)
+  contents = res.snippets
+
+  return " ".join(content.text for content in contents)
