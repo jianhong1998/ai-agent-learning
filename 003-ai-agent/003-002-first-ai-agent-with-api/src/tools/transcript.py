c78b547173b72c4e69327037a6e5d14448c73589
Jason Lee <77224948+jianhong1998@users.noreply.github.com>
2026-01-17 11:18:44 +0800

    learn: 003 - build ai agent with X.Ai SDK and Fastapi (#1)
    * create base project
    
    * create agent
    
    * create 002 with api endpoint
    
    * add fast API to trigger AI agent
    
    * enhance error handling
    
    * setup code formatter

---
+++ 003-ai-agent/003-002-first-ai-agent-with-api/src/tools/transcript.py
@@ -0,0 +1,19 @@
+from helpers.transcript import get_transcript as get_transcript_helper
+
+
+def get_transcript(url: str, languages: list[str] = ['en']) -> str:
+  """
+  Get transcription for a youtube video.
+
+  :param url: Youtube video URL. Example: `https://www.youtube.com/watch?v=zOFxHmjIhvY`
+  :type url: str
+  :param languages: List of languages for the transcipt.
+  :type languages: list[str]
+  :return: Transcription of the Youtube video.
+  :rtype: str
+  """
+
+  if len(languages):
+    languages.append('en')
+
+  return get_transcript_helper(url, languages)
