fa26507d84cfdc6ee3baa6f408cc415ed5265b53
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2026-01-24 15:57:56 +0800

    change model

--- 004-langchain/004-001-basic-langchain/src/custom_agents/agents/ollama.py
+++ 004-langchain/004-001-basic-langchain/src/custom_agents/agents/ollama.py
@@ -6,7 +6,7 @@ from langchain_ollama import ChatOllama
 
 
 def init_model(tools: list[BaseTool]) -> Runnable[LanguageModelInput, AIMessage]:
-  model = ChatOllama(model='qwen3:1.7b', base_url='http://localhost:11434', temperature=0.5)
+  model = ChatOllama(model='qwen3:4b', base_url='http://localhost:11434', temperature=0.5)
 
   model_with_tool = model.bind_tools(tools=tools, tool_choice='auto')  # type: ignore
 
